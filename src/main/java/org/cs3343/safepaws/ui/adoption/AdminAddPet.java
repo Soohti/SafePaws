472e3415502ee2dc166cd00ebf583938bc8f76c1
zyucidam <yucizym@outlook.com>
2024-11-30 16:45:48 +0800

    -implement orm

--- src/main/java/org/cs3343/safepaws/ui/adoption/AdminAddPet.java
+++ src/main/java/org/cs3343/safepaws/ui/adoption/AdminAddPet.java
@@ -1,7 +1,6 @@
 package org.cs3343.safepaws.ui.adoption;
 
 import org.cs3343.safepaws.entity.Admin;
-import org.cs3343.safepaws.entity.Pet;
 import org.cs3343.safepaws.util.CreatePetRecordHandler;
 import org.cs3343.safepaws.handler.PetRegisterHandler;
 import org.cs3343.safepaws.ui.UI;
