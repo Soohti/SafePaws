19988ad195fe536ce8bed4be044a1533c7a4beeb
zyucidam <yucizym@outlook.com>
2024-12-06 17:51:32 +0800

    javadoc

--- src/main/java/org/cs3343/safepaws/entity/package-info.java
+++ src/main/java/org/cs3343/safepaws/entity/package-info.java
@@ -1,4 +1,7 @@
 /**
- * TODO.
+ * This package contains the entity classes for the SafePaws application.
+ * Entities represent the core data model of the application, encapsulating
+ * the properties and behavior of the various objects that the application
+ * manages.
  */
 package org.cs3343.safepaws.entity;
--- src/main/java/org/cs3343/safepaws/ui/shelter/package-info.java
+++ src/main/java/org/cs3343/safepaws/ui/shelter/package-info.java
@@ -1,4 +1,8 @@
 /**
- * TODO.
+ * This package contains the user interface (UI) pages specifically
+ * designed for shelter users
+ * in the SafePaws application. It includes classes and resources that
+ * build and manage the UI
+ * components and layouts for shelter-related functionalities.
  */
 package org.cs3343.safepaws.ui.shelter;
